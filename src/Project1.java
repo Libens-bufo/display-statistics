9c7d5a1aaaa2eb19077b827aaad44890ee0af7bc
Jason Swinehart <jaswine@comcast.net>
2022-09-28 22:01:29 -0400

    initial commit

---
+++ src/Project1.java
@@ -0,0 +1,248 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Project1 {
+    //globals
+
+    static String[] input; //user input
+    static int[] inputs; //array of integers parsed from input
+
+    static int count, mode, min, max, range, even, odd, prime;
+    static double mean, median, variance, stdev;
+    public static String[] readList(){
+        Scanner in = new Scanner(System.in);
+
+        // Read user input
+        String input = in.nextLine();
+
+        //Convert string input into usable integer array
+        String[] inputSplit = input.split(" ");
+        return inputSplit;
+
+    }
+    public static int[] getNums(String[] str, int length){
+        int[] arr = new int[length];
+        Scanner stdin = new Scanner(System.in);
+        for (int i = 0; i < length; i++){
+           arr[i] = Integer.parseInt(str[i]);
+       }
+        return arr;
+    }
+    static void selectOptions(int[] arr){
+
+    }
+
+    /**
+     * Displays the statistics
+     * @param arr The user inputs
+     */
+    public static void displayStats(int[] arr){
+
+        getStats(arr);
+
+        //Print statistics
+        System.out.printf("" +
+                        "Min: " + min + "\n" +
+                        "Max: " + max + "\n" +
+                        "Count: " + count + "\n" +
+                        "Range: " + range + "\n" +
+                        "Median " + median + "\n" +
+                         "Mean: " + mean + "\n"
+        );
+
+        // Print mode
+        if ((mode > -1)) {
+            System.out.println("Mode: " + mode);
+        } else {
+            System.out.println("Mode: No Mode");
+        }
+
+        //Print Variance and Standard Deviation
+        System.out.printf("" +
+                "Variance %.2f\n" +
+                "Standard Deviation %.2f\n",
+                variance,
+                stdev
+        );
+    }
+
+    /**
+     * Updates global variables containing the statistics
+     * @param arr The array of user inputs
+     */
+    public static void getStats(int[] arr){
+        count = count(arr);
+        mean = mean(arr);
+        median = median(arr);
+        mode = (mode(arr) > -1) ? arr[mode(arr)] : -1;
+        min = min(arr);
+        max = max(arr);
+        range = range(arr);
+        variance = variance(arr);
+        stdev = stDev(arr);
+
+        //Math.round((variance(arr)) * 100.0) / 100.0;
+    }
+
+    /**
+     * Returns the count of user inputs
+     * @param arr The array of inputs
+     * @return the count of elements(inputs)
+     */
+    public static int count (int[] arr){
+        return arr.length;
+    }
+
+    /**
+     *This function calculates the mean of an array of integers.
+     * @param arr The array of inputs
+     * @return the mean of the inputs
+     */
+    public static double mean(int[] arr){
+        double total = 0;
+        for(int i = 0; i <  arr.length; i++){
+            total += arr[i];
+        }
+        return total / arr.length;
+    }
+
+    /**
+     * Calculates the median of the user inputted integers
+     *
+     * @param arr The user inputs
+     * @return the median
+     */
+    public static double median(int[] arr){
+        double med;
+        int mid = arr.length / 2;
+        Arrays.sort(arr);
+
+        if ((arr.length % 2) == 0){
+            med = (arr[mid] + arr[mid-1]) / 2.0;
+        }else{
+            med = arr[mid];
+        }
+        return med;
+    }
+
+    /**
+     * Determines if there is a mode
+     *
+     * @param arr The user inputs
+     * @return Index of the mode, or -1 if no mode.
+     */
+    public static int mode(int[] arr){
+        int maxCount = 1;
+        int count;
+        int index = -1;
+        for(int i = 0; i <= arr.length - 1; i++){
+            count = 0;
+            for(int j = 0; j < arr.length; j++){
+                if (arr[j] == arr[i]) count++;
+            }
+            if (count > maxCount){
+                maxCount = count;
+                index = i;
+            }
+        }
+        return index;
+
+    }
+
+    public static int max(int[] arr){
+        int maximum = arr[0];
+        for(int el : arr){
+            maximum = Math.max(el, maximum);
+        }
+        return maximum;
+    }
+    public static int min(int[] arr){
+        int minimum = arr[0];
+        for(int el : arr){
+            minimum = Math.min(el, minimum);
+        };
+        return minimum;
+    }
+
+    public static int range(int[] arr){
+        int minimum = min(arr), maximum = max(arr);
+        if (minimum > -1) return maximum - minimum;
+        else return maximum + Math.abs(minimum);
+
+    };
+
+    public static double variance(int[] arr){
+        double result = 0;
+        double mean = mean(arr);
+        for(int i = 0; i < arr.length; i++){
+            result += Math.pow(arr[i] - mean, 2);
+        }
+        return result / arr.length;
+    }
+
+    public static double stDev(int[] arr){
+        return Math.pow(variance(arr), .5);
+        //return Math.ceil(Math.pow(variance(arr), .5) * 100.0) / 100.0;
+    };
+    public static void main(String[] args) {
+        input = readList();
+        inputs = getNums(input, input.length);
+
+        for (int i = 0; i < inputs.length; i++){
+            System.out.print("Num: " + inputs[i]+" ");
+        }
+
+        System.out.printf("\n" +
+                "Please make a selection: \n" +
+                "1) Display List Statistics\n" +
+                "2) Display List Ordered\n" +
+                "3) Number of Odd/Even\n" +
+                "4) Check for Prime Numbers\n" +
+                "5) Enter New List\n" +
+                "exit) Quit Program\n"
+        );
+        Scanner stdin = new Scanner(System.in);
+        String selection = stdin.next();
+
+        if (selection.equals("1")) { //Display List Stats
+            displayStats(inputs);
+        }
+        else if (selection.equals("2")){ // Display List Ordered
+            displayOrderedList(inputs);
+        }
+        else if (selection.equals("3")){ //Number of Odd / Even
+
+        }
+        else if (selection.equals("4")){ //Check for primes
+
+        }
+        else if (selection.equals("5")){ //Enter a new list
+            input = readList();
+            inputs = getNums(input, input.length);
+            for (int i = 0; i < inputs.length; i++){
+                System.out.print("Num: " + inputs[i]+" ");
+            }
+        }
+        else if (selection.equals("exit")){ //Exits program with status code 0
+            System.out.print("Program Exiting");
+            System.exit(0);
+
+        }
+
+
+
+    }
+
+    private static void displayOrderedList(int[] arr) {
+        Arrays.sort(arr);
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++ ){
+            System.out.print("" + arr[i]);
+            if (i != arr.length - 1) System.out.print(", ");
+        };
+        System.out.print("]");
+    }
+
+
+}
